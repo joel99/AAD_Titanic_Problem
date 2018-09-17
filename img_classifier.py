52175a310be4ffcf2678fcd711b2c009eb39a124
aaronmcdaniel <aaronmcdaniel7@gmail.com>
2018-09-17 17:34:51 -0400

    Remeved extra code from framework code

--- img_classifier.py
+++ img_classifier.py
@@ -17,9 +17,6 @@ class ImageClassifier:
     def __init__(self):
         self.classifier = None
 
-    def imread_convert(self, f):
-        return io.imread(f).astype(np.uint8)
-
     def load_data_from_folder(self, dir):
         # read all images into an image collection
         ic = io.ImageCollection(dir+"*.bmp", load_func=self.imread_convert)
@@ -34,26 +31,6 @@ class ImageClassifier:
             labels[i] = f[len(dir):m.start()]
 
         return(data,labels)
-
-    def extract_image_features(self, data, cell=51, block=2, norm='L2'):
-        # Please do not modify the header above
-
-        # extract feature vector from image data
-        feature_data = []
-        for img in data:
-            #hog implementation -> 97.5% cell:(12,12) block:(1,1) L1-sqrt TOO EASY
-            if type(img[0][0]) == list or type(img[0][0]) == np.array or type(img[0][0]) == np.ndarray:
-                img = np.array([[value[0] for value in row] for row in img])
-            else:
-                img = np.array([[value for value in row] for row in img])
-            f = feature.hog(img, orientations=10, pixels_per_cell=(cell, cell), cells_per_block=(block, block), feature_vector=True, block_norm=norm)
-            feature_data.append(f)
-            #feature_data += feature.hog(img, orientations=10, pixels_per_cell=(48, 48), cells_per_block=(4, 4), feature_vector=True, block_norm='L2-Hys')
-            # has 100% accuracy
-            #feature_data += [hog(img, pixels_per_cell=(cell,cell),cells_per_block=(block,block),block_norm=norm)]
-		#
-        # Please do not modify the return type below
-        return(feature_data)
 
     def train_classifier(self, train_data, train_labels):
         # Please do not modify the header above
--- imgclassifier_eval.py
+++ imgclassifier_eval.py
@@ -15,31 +15,20 @@ from PIL import Image
 
 
 import time
-
-def print_pic(img):
-    #for 3D or 2D grey image
-    #if type(img[0][0]) is np.array or type(img[0][0]) is np.ndarray or type(img[0][0]) is list: 
-    #    img = Image.fromarray(img, "RGB")
-    #else:
-    img = Image.fromarray(img)
-    img.show()
  
 def main():
     img_clf = ImageClassifier()
     start_true = time.time()
     
     #param tuning
-    max = 0.0
-    k = 5
-    cells = [51]
-    blocks = [2]
-    display = False
-    norms = ['L2']
-    best = [cells[0], blocks[0], norms[0]]
+    p1 = [0,2,3]
+    p2 = [4,5,6]
+    best = [p1[0], p2[0]] # list of best params
 
     
+    # Write headers
     file_name = "classifier/results/results.csv"
-    heading = "Hog Results of Param Search and Cross Validation Scores\ncells,blocks,norm,Image Time, Train Time, Avg Test, F1 Test, F2 Test, F3 Test, F4 Test, F5 Test\n"
+    heading = "Title\nFalse Positive, False Negative, Train Time, Avg Test, F1 Test, F2 Test, F3 Test, F4 Test, F5 Test\n"
     file = open(file_name,'w')
     file.write(heading)
     file.close()
@@ -47,36 +36,19 @@ def main():
     (data_raw, data_labels) = img_clf.load_data_from_folder('./classifier/data/')
 
     
-    if not display: # block searching for best parameters based on cross validation
-        for n in norms:
-            for b in blocks:
-                for c in cells:
+    # block searching for best parameters based on cross validation
+    for one in p1:
+        for two in p2:
                     start = time.time()
-                    # automate parameter tuning
-                    # load images
-
-                    print("\n*********************************************\nProcessing Images")
-                    print("Cells: %d\tBlocks: %d\tNorm: %s\n" %(c, b, n))
-                    # convert images into features
-                    feature_data = img_clf.extract_image_features(data_raw, c, b, n)
-                    end = time.time()
-                    image_time = end - start
-                    print("Finished Loading images in %f seconds" %(end-start))
-
-                    # test model
-
-                    line = "%d,%d,%s,%f," %(c, b, n, image_time)
-                    # implement heap data
-                    # convert images into features
-                    #print("TRD: %s\tTED: %s\tTRL: %s\tTEL: %s" %(type(train_data),type(test_data),type(train_labels),type(test_labels)))
-                    accs = []
-                    test = 0
+                    
+                    line = "%d,%d,%s,%f," %(c, b, n, image_time) # writing parameters used in line
+                    accs = [] # List of accuracies by fold
                     for fold in range(k):
                         train_data = []
                         test_data = []
                         train_labels = []
                         test_labels = []
-                        #rearange data
+                        #rearange data by fold
                         for i in range(len(feature_data)):
                             if i%k == fold:
                                 test_data += [feature_data[i]]
@@ -86,32 +58,26 @@ def main():
                                 train_labels += [data_labels[i]]
                     
                         # train model and test on training data
-                        try:
-                            img_clf.train_classifier(train_data, train_labels)
-                        except:
-                            train_data = [[0] for img in train_data]
-                            img_clf.train_classifier(train_data, train_labels)
+                        img_clf.train_classifier(train_data, train_labels)
 
-                        print("Trained Fold %d in %f seconds" %(fold,(time.time()-end)))
+                        # document train time
+                        print("Trained Fold %d in %f seconds" %(fold,(time.time()-end))) 
                         train_time = time.time()-end
                         if fold == 0:
                             line += "%f," %train_time
 
                         # test model
-                        try:
-                            predicted_labels = img_clf.predict_labels(test_data)
-                        except:
-                            test_data = [[0] for d in test_data]
-                            predicted_labels = img_clf.predict_labels(test_data)
-                        #print("Testing results:")
-                        #print("=============================")
-                        #print("Confusion Matrix:\n",metrics.confusion_matrix(test_labels, predicted_labels))
+                        predicted_labels = img_clf.predict_labels(test_data)
+                        
+                        
+                        print("Testing results:")
+                        print("=============================")
                         print("Accuracy: ", metrics.accuracy_score(test_labels, predicted_labels))
                         print("F1 score: ", metrics.f1_score(test_labels, predicted_labels, average='micro'))
-                        print("Cells: %d\tBlocks: %d\tNorm: %s\n" %(c, b, n))
-                        print("Train Length: %d\tTest Length: %d" %(len(train_data), len(test_data)))
+                        print("Param 1: %d\tParam 2: %d\n" %(one, two))
                         test_accuracy = metrics.accuracy_score(test_labels, predicted_labels)
-                        accs += [test_accuracy]
+                        #should have FP and FN
+                        accs += [test_accuracy] # adds to list of fold accuracies
                         
 
                     avg = sum(accs)/k
@@ -120,6 +86,7 @@ def main():
                     file.write(line)
                     file.close()
                         
+                    # document best model
                     if avg > max:
                         max = avg
                         best = [c, b, n]
@@ -142,18 +109,6 @@ def main():
         f.write("Best configuration\nC: %d\tB: %d\tN: %s" %(best[0],best[1],best[2]))
         f.close()
         print("Successfully Saved Model")
-
-    else:
-        for i in [1,5]:#,10,15,20,25,30,34,35]:
-            img = np.array([[value[0] for value in row] for row in test_raw[i]])
-            # show orig
-            #print_pic(img)
-            # show sobel image
-            print_pic(img)
-            print_pic(center_pic(img))
-            # show centered image
-            #print_pic(center_pic(test_raw[i]))
-  
             
     print("\nTotal Time: %f seconds" %(time.time()-start_true))
     
