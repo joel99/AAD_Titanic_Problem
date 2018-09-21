0985c1ee61c0e86eff6562bae251a499c70347d0
Joshua Reno <joshuareno@lawn-128-61-66-26.lawn.gatech.edu>
2018-09-21 13:49:06 -0400

    final gnb

--- gnb.py
+++ gnb.py
@@ -1,12 +1,11 @@
-# import numpy as np
-# import re
 from sklearn.naive_bayes import GaussianNB
-# from skimage import io, feature, filters, exposure, color
 from parser import load_split_all
 from sklearn.model_selection import cross_val_score
 
 def gaussianNB():
+    classifier = GaussianNB()
     x, y = load_split_all()
-    return cross_val_score(GaussianNB(), x, y)
+    print(cross_val_score(classifier, x, y))
+    return classifier
 
 gaussianNB()
