262e5645f5a457e6134a0853e82eadc9b58c9c7b
aaronmcdaniel <aaronmcdaniel7@gmail.com>
2018-09-21 18:06:08 -0400

    added method for selecting best model for SVM

--- classifier_svm_eval.py
+++ classifier_svm_eval.py
@@ -17,8 +17,6 @@ path = './data/'
 
 def main():
     clf = Classifier()
-    start_true = time.time()
-    
 
     #param tuning
     kernals = ['linear','poly','rbf','sigmoid']
@@ -28,14 +26,6 @@ def main():
     folds = 5
     
     best = [kernals[0], probabilities[0], tols[0], gammas[0]] # list of best params
-    
-    # Write headers
-    file_name = "./results/svm_results.csv"
-    heading = "SVM classifier performance\nKernal,Probability,Tol,Gamma,Avg FP,Avg FN,Total,Avg Accuracy,Train Time\n"
-    file = open(file_name,'w')
-    file.write(heading)
-    file.close()
-    
     (data, labels) = read_data()
 
     # block searching for best parameters based on cross validation
@@ -46,78 +36,46 @@ def main():
                 for g in gammas:
                     start = time.time()
                     
-                    line = "%s,%s,%f,%f," %(k, p, t, g) # writing parameters used in line
-                    accs = [] # List of accuracies by fold
                     fps = [] # List of false positives by fold
                     fns = [] # List of false negatives by fold
                     for fold in range(folds):
                         (train_data, train_labels, test_data, test_labels) = make_fold(data,labels,folds,fold)
                     
                         # train model and test on training data
-                        clf.train_classifier_SVM(train_data, train_labels, k, p, t, g)
-
-                        # document train time
-                        print("Trained Fold %d in %f seconds" %(fold,(time.time()-end))) 
-                        train_time = time.time()-end
-                        if fold == 0:
-                            line += "%f," %train_time
+                        clf.create_SVM(k,p,t,g)
+                        clf.train(train_data, train_labels)
 
                         # test model
-                        predicted_labels = clf.predict_labels(test_data)
-                        
-                        # display testing results
-                        print("Testing results:")
-                        print("=============================")
-                        print("Accuracy: ", metrics.accuracy_score(test_labels, predicted_labels))
-                        print("F1 score: ", metrics.f1_score(test_labels, predicted_labels, average='micro'))
-                        print("Param 1: %d\tParam 2: %d\n" %(one, two))
-                        test_accuracy = metrics.accuracy_score(test_labels, predicted_labels)
-                
+                        predicted_labels = clf.predict(test_data)
+
                         # calculate FP and FN
                         total = len(test_data)
                         fp = sum([1 for i in range(total) if predicted_labels[i] == 1 and test_labels[i] == 0])
                         fn = sum([1 for i in range(total) if predicted_labels[i] == 0 and test_labels[i] == 1])
-                        
-                        accs += [test_accuracy] # adds to list of fold accuracies
                         fps += [fp]
                         fns += [fn]
-                        
 
-                    avgA = sum(accs)/folds
                     avgFP = sum(fps)/folds
                     avgFN = sum(fns)/folds
-                    line += "%f,%f,%d,%f\n" %(avgFP, avgFN, int(len(data)/folds), avgA)
-                    file = open(file_name, 'a')
-                    file.write(line)
-                    file.close()
-                        
+                    score = (avgFP + avgFN) / 2
+
                     # document best model
-                    if avg > max:
-                        max = avg
+                    if score > max:
+                        max = score
                         best = [k,p,t,g]
-                    #print("\nCell: %d\tBlock: %d\tNorm: %s" %(c,b,n))
-                    print("Avg Accuracy: %f\tAvg FP: %f\tAVG FN: %f" %(avgA,avgFP,avgFN))
-                    print("Best Avg Accuracy: %f" %max)
-                    print("Current Duration: %f seconds" %(time.time()-start_true))
+                    print("Params\nk: %s\tp: %s\tt: %f\tg: %f" %(k,p,t,g))
+                    print("Avg FP: %f\tAVG FN: %f\ttotal: %d" %(avgFP,avgFN,len(data)))
+                    print("Best Avg Score: %f" %max)
                     print("*********************************************")
                     
-                    #only change gamma if it is relavent
+                    #only change gamma if it is relavent (SVM specific)
                     if gam_irrelevent:
                         break
                     
-        #save model with best avg accuracy
-        print("Saving best model")
-        print("PARAMS:\nKernal: %s\tProb: %s\tTol: %f\tGamma: %f" %(best[0],best[1],best[2],best[3]))
-        (all_data, all_labels) = read_data()
-        clf.train_classifier(all_data,all_labels,best[0],best[1],best[2],best[3])
-        clf.save("./models/best.pkl")
-        f = open("./models/best.txt", 'w')
-        f.write("Best configuration\nK: %s\tP: %s\tT: %f\tG: %f" %(best[0],best[1],best[2],best[3]))
-        f.close()
-        print("Successfully Saved Model")
-            
-    print("\nTotal Time: %f seconds" %(time.time()-start_true))
-    
+        #return best model
+        return clf.create_SVM(best[0],best[1],best[2],best[3])
+
+
 if __name__ == "__main__":
     main()
 
@@ -133,6 +91,9 @@ def read_data():
     labels = np.array(list(train_labels) + list(test_labels))
     labels = np.array([l[0] for l in labels])
 
+    data = np.delete(data, [0, 2, 7, 9, 10], axis=1)
+
+
     #does not handle strings
     return (data, labels)
 
@@ -143,7 +104,7 @@ def filter_strings(data):
     # replaces all letters with their location in the alphabet (i.e. 'a' -> 0, 'ab' -> 01)
     for line in data:
         for i in range(len(data)):
-
+            #res
 
 def make_fold(data, labels, total, sel):
     # turns data and labels into train and test sets
@@ -164,4 +125,25 @@ def make_fold(data, labels, total, sel):
 
     return (train_data, train_labels, test_data, test_labels)
 
+
+def load_split_all():
+    le = LabelEncoder()
+    train_data = load_data(train_fn)
+
+    # Note test data has different data order
+    # Convert gender column (col 4)
+    le.fit(["male", "female"])
+    train_data[:, 4] = le.transform(train_data[:, 4])
+
+    # Feature selection, trim passenger_id (col 0), label (col 1) and name (col 3), embark location, cabin number, ticket number
+    train_data = np.delete(train_data, [0, 3, 8, 10, 11], axis=1)
+
+    # Drop NaN for now. See imputing
+    train_data = train_data[~pd.isnull(train_data).any(axis=1)]
+    x_train = train_data[:, 1:].astype('float')
+    y_train = train_data[:, 0].astype('int')
+    xtr, xte, ytr, yte = train_test_split(x_train, y_train)
+    # kf = KFold(n_splits=folds)
+    # indices = kf.split(train_data, test_data)
+    return xtr, xte, ytr, yte  # Ideally would like an interface to request organized data
 
