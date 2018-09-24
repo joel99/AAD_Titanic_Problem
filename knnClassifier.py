d7779039f243e5d76e5ab6743d1d587db986d113
Jeffrey Minowa <jeffminowa@gatech.edu>
2018-09-24 02:59:22 -0400

    add plots to driver

--- knnClassifier.py
+++ knnClassifier.py
@@ -6,24 +6,15 @@ def find_best_knn(data, labels):
     neighbors = list(range(1,50))
     (fig, sc) = parser.init_graph()
     front = []
-    ind = 0
     for k in neighbors:
         knn = KNeighborsClassifier(n_neighbors=k)
         scores = parser.score(knn, data, labels)
         precision, recall = scores
         score = parser.convert_to_FP_FN(labels, precision, recall)
-        print "FP_FN scores:", score
         individual = [score, [k]]
         front = parser.update_front(front, individual, parser.pareto_dominance_min)
-        print len(front)
-        ind+=1
-        print ind
-
         parser.update_graph(fig, sc, front)
-    try:
-        plt.waitforbuttonpress()
-    except:
-        print("Done")
+
     return generate_KNN_front(front)
 
 def generate_KNN_front(front):
